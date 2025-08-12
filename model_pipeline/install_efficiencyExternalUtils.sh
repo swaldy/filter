80e6ec81d94ef623f1c77b15061ba750c7f559a0
CodexForster <dsheka3@uic.edu>
2025-08-12 10:46:02 -0500

    testing .gitmodules

--- model_pipeline/install_efficiencyExternalUtils.sh
+++ model_pipeline/install_efficiencyExternalUtils.sh
@@ -1,2 +1,6 @@
-git clone https://github.com/smart-pix/pretrain-data-prep.git
-cp pretrain-data-prep/dataset_utils.py ./
+git clone --recursive https://github.com/smart-pix/filter.git
+git submodule foreach --recursive 'if git show-ref --verify --quiet refs/heads/main; then git checkout main; elif git show-ref --verify --quiet refs/heads/master; then git checkout master; else echo "Neither main nor master branch exists"; fi'
+git submodule foreach --recursive git pull
+
+# git clone https://github.com/smart-pix/pretrain-data-prep.git
+# cp pretrain-data-prep/dataset_utils.py ./
