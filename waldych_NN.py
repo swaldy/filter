568eb6d0e58e7a7f450bb9db45ae4d436274adf1
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-01-30 12:04:11 -0600

    Update waldych_NN.py

--- waldych_NN.py
+++ waldych_NN.py
@@ -1,1 +1,23 @@
+%env TF_USE_LEGACY_KERAS 1
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn import datasets, svm, metrics
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.models import load_model
+# import keras
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pandas import read_csv
+import math
+from tensorflow.keras.optimizers import Adam
 
