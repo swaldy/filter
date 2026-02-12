6819d43e8f54f662634a1a281a464abd187f9930
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-02-12 11:58:09 -0600

    Update waldych_preprocess.py

--- waldych_preprocess.py
+++ waldych_preprocess.py
@@ -24,12 +24,12 @@ iter=0
 suffix = 16400
 for filepath in glob.iglob(dirtrain+'labels*.parquet'):
         iter+=3
-    output_file.write(str(iter)+" files present in directory."+"\n")
-    for i in range(int(iter/3)):
-            trainlabels.append(pd.read_parquet(dirtrain+'labels_d'+str(suffix+i+1)+'.parquet'))
-            trainrecons.append(pd.read_parquet(dirtrain+'recon2D_d'+str(suffix+i+1)+'.parquet'))
-    trainlabels_csv = pd.concat(trainlabels, ignore_index=True)
-    trainrecons_csv = pd.concat(trainrecons, ignore_index=True)
+        output_file.write(str(iter)+" files present in directory."+"\n")
+for i in range(int(iter/3)):
+        trainlabels.append(pd.read_parquet(dirtrain+'labels_d'+str(suffix+i+1)+'.parquet'))
+        trainrecons.append(pd.read_parquet(dirtrain+'recon2D_d'+str(suffix+i+1)+'.parquet'))
+        trainlabels_csv = pd.concat(trainlabels, ignore_index=True)
+        trainrecons_csv = pd.concat(trainrecons, ignore_index=True)
 
 iter_0, iter_1, iter_2 = 0, 0, 0
 iter_rem = 0
