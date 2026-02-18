b876ed52dba609a1539befd780da1d9ac05f5887
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-02-18 12:24:25 -0600

    Update Waldych_BDT.py

--- Waldych_BDT.py
+++ Waldych_BDT.py
@@ -1,6 +1,7 @@
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
 sensor_geom = "50x12P5x150_0fb"
 threshold = 0.2 #in GeV
