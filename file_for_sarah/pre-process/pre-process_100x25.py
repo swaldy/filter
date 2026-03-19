3a1b3ef74d75e379f04c2e9d4f10357a6339ffe3
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-03-18 20:48:13 -0500

    s

--- file_for_sarah/pre-process/pre-process_100x25.py
+++ file_for_sarah/pre-process/pre-process_100x25.py
@@ -11,7 +11,7 @@ directory = "/eos/user/s/swaldych/smart_pix/preprocess_output/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-sensor_geom = '100x12P5'
+sensor_geom = '100x25'
 output_file = open(directory+"output"+sensor_geom+".txt", "w")
 
 # for thresh_iter in [0.2]:
